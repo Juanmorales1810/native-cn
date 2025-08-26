4c196da2579e4364707b8d12935d43524db2ef1f
Agustina <85465719+Juanmorales1810@users.noreply.github.com>
2025-08-26 12:16:31 -0300

    refactor: mejorar la estructura y legibilidad del componente RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,20 +6,40 @@ import { PortalHost } from '@rn-primitives/portal';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'nativewind';
+import React from 'react';
+import { KeyboardAvoidingView, Platform, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+    // Catch any errors thrown by the Layout component.
+    ErrorBoundary,
 } from 'expo-router';
 
 export default function RootLayout() {
-  const { colorScheme } = useColorScheme();
+    const { colorScheme } = useColorScheme();
+    const insets = useSafeAreaInsets();
 
-  return (
-    <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
-      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-      <Stack />
-      <PortalHost />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
+            {Platform.OS === 'ios' ? (
+                <KeyboardAvoidingView
+                    style={{ flex: 1 }}
+                    behavior="padding"
+                    keyboardVerticalOffset={insets.top}
+                    enabled>
+                    <View style={{ flex: 1 }}>
+                        <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+                        <Stack />
+                        <PortalHost />
+                    </View>
+                </KeyboardAvoidingView>
+            ) : (
+                <View style={{ flex: 1 }}>
+                    <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+                    <Stack />
+                    <PortalHost />
+                </View>
+            )}
+        </ThemeProvider>
+    );
 }
